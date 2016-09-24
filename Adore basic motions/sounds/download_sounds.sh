4ac80f4e21680841f7e093f9d8d2d6c368b27d40
Damjan Miklic <damjan.miklic@gmail.com>
2016-09-24 16:05:38 +0200

    Added script for downloading sounds.

---
+++ Adore basic motions/sounds/download_sounds.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "Downloading sounds..."
+
+# Pozrav "Bok", tts Marica v4.2
+wget --no-check-certificate -O bok.wav "http://larics.rasip.fer.hr/laricscloud/index.php/s/bKCevCzwyqNRo87/download"
+
+# Pozdrav "Bravo", tts Marics v4.2
+wget --no-check-certificate -O bravo.wav "http://larics.rasip.fer.hr/laricscloud/index.php/s/XL7u7FJybk9E0TC/download"
+
+# Pozdrav "Bravo", tts Marics v4.2
+wget --no-check-certificate -O dodji.wav "http://larics.rasip.fer.hr/laricscloud/index.php/s/YBEL3LwFh6LYAfS/download"
+
+# Pozdrav "Bravo", tts Marics v4.2
+wget --no-check-certificate -O jakodobro.wav "http://larics.rasip.fer.hr/laricscloud/index.php/s/DoYFHrfEiIrq6LC/download"
+
+# Pozdrav "Bravo", tts Marics v4.2
+wget --no-check-certificate -O vidi.wav "http://larics.rasip.fer.hr/laricscloud/index.php/s/mB24a8VFmIdWgiM/download"
+
+echo "Done!"
